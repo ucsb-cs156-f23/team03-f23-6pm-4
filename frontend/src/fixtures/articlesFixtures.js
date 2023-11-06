c0a0a9ffc9784434c092c176b7c906815185145e
David Wang <david.wang5766@gmail.com>
2023-11-05 23:23:42 -0800

    Changing oneArticle for articlesFixtures to be a single object and correcting name to be plural

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,40 +1,39 @@
-const articlesFixture = {
-  oneArticle: [
+const articlesFixtures = {
+    oneArticle:
     {
-      id: 1,
-      title: "event1",
-      url: "nytimes.com",
-      explanation: "example",
-      email: "author@nytimes.com",
-      dateAdded: "",
+        id: 1,
+        title: "event1",
+        url: "nytimes.com",
+        explanation: "example",
+        email: "author@nytimes.com",
+        dateAdded: "",
     },
-  ],
-  threeArticles: [
-    {
-      id: 1,
-      title: "event1",
-      url: "nytimes.com",
-      explanation: "example",
-      email: "author@nytimes.com",
-      dateAdded: "2023-05-31T00:00:00",
-    },
-    {
-      id: 2,
-      title: "event2",
-      url: "latimes.com",
-      explanation: "also an example",
-      email: "writer@latimes.com",
-      dateAdded: "2010-01-01T01:23:45",
-    },
-    {
-      id: 3,
-      title: "event3",
-      url: "washingtonpost.com",
-      explanation: "still an example",
-      email: "poet@washingtonpost.com",
-      dateAdded: "2003-05-31T00:00:00",
-    },
-  ],
+    threeArticles: [
+        {
+            id: 1,
+            title: "event1",
+            url: "nytimes.com",
+            explanation: "example",
+            email: "author@nytimes.com",
+            dateAdded: "2023-05-31T00:00:00",
+        },
+        {
+            id: 2,
+            title: "event2",
+            url: "latimes.com",
+            explanation: "also an example",
+            email: "writer@latimes.com",
+            dateAdded: "2010-01-01T01:23:45",
+        },
+        {
+            id: 3,
+            title: "event3",
+            url: "washingtonpost.com",
+            explanation: "still an example",
+            email: "poet@washingtonpost.com",
+            dateAdded: "2003-05-31T00:00:00",
+        },
+    ],
 };
 
-export { articlesFixture };
+export { articlesFixtures };
