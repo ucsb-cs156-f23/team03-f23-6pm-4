d8084d5032359d49c4b8d6139e2ea6ca724af473
David Wang <david.wang5766@gmail.com>
2023-11-07 14:51:59 -0800

    56 - Added ArticlesTable component with tests and stories

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -2,27 +2,27 @@ import React from "react";
 import OurTable, { ButtonColumn } from "main/components/OurTable";
 
 import { useBackendMutation } from "main/utils/useBackend";
-import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/articlesUtils"
 import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
-export default function UCSBDatesTable({ dates, currentUser }) {
+export default function ArticlesTable({ articles, currentUser }) {
 
     const navigate = useNavigate();
 
     const editCallback = (cell) => {
-        navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+        navigate(`/articles/edit/${cell.row.values.id}`)
     }
 
     // Stryker disable all : hard to test for query caching
-
     const deleteMutation = useBackendMutation(
         cellToAxiosParamsDelete,
         { onSuccess: onDeleteSuccess },
-        ["/api/ucsbdates/all"]
+        ["/api/articles/all"]
     );
     // Stryker restore all 
 
+    
     // Stryker disable next-line all : TODO try to make a good test for this
     const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
@@ -33,27 +33,35 @@ export default function UCSBDatesTable({ dates, currentUser }) {
             accessor: 'id', // accessor is the "key" in the data
         },
         {
-            Header: 'QuarterYYYYQ',
-            accessor: 'quarterYYYYQ',
+            Header: 'Title',
+            accessor: 'title',
         },
         {
-            Header: 'Name',
-            accessor: 'name',
+            Header: 'URL',
+            accessor: 'url',
         },
         {
-            Header: 'Date',
-            accessor: 'localDateTime',
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Date Added',
+            accessor: 'dateAdded',
         }
     ];
 
     if (hasRole(currentUser, "ROLE_ADMIN")) {
-        columns.push(ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"));
-        columns.push(ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable"));
+        columns.push(ButtonColumn("Edit", "primary", editCallback, "ArticlesTable"));
+        columns.push(ButtonColumn("Delete", "danger", deleteCallback, "ArticlesTable"));
     }
 
     return <OurTable
-        data={dates}
+        data={articles}
         columns={columns}
-        testid={"UCSBDatesTable"}
+        testid={"ArticlesTable"}
     />;
 };
