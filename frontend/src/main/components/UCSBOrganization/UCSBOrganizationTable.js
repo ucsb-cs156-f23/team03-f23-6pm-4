1242fd745aae30e0dc4b5dfdaf41d02b32c0fb90
allychu <allychu@umail.ucsb.edu>
2023-11-13 04:27:03 -0800

    fixed duplicate access for UCSBOrganization Table

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -42,7 +42,7 @@ export default function UCSBOrganizationTable({ orgs, currentUser }) {
         },
         {
             Header: 'inactive',
-            accessor: 'inactive',
+            id: 'inactive',
             accessor: (row, _rowIndex) => String(row.inactive)
         }
     ];
